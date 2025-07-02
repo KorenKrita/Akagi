a6c76045c952a41f1b9c7bc414b7774641772b62
shinkuan <shinkuan318@gmail.com>
2025-07-02 23:02:56 +0800

    Remove unused bridge imports and update __all__ in __init__.py

--- playwright_client/bridge/__init__.py
+++ playwright_client/bridge/__init__.py
@@ -1,8 +1,5 @@
 # __init__.py
 from .bridge_base import BridgeBase
-from .amatsuki import AmatsukiBridge
 from .majsoul import MajsoulBridge
-from .riichi_city import RiichiCityBridge
-from .tenhou import TenhouBridge
 
-__all__ = ['BridgeBase', 'AmatsukiBridge', 'RiichiCityBridge', 'MajsoulBridge', 'TenhouBridge']
+__all__ = ['BridgeBase', 'MajsoulBridge']
