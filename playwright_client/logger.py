5e28be739ff0945383d1416df744528b0a6e2efa
shinkuan <shinkuan318@gmail.com>
2025-07-02 23:02:38 +0800

    Rename to playwright

--- playwright_client/logger.py
+++ playwright_client/logger.py
@@ -5,7 +5,7 @@ from loguru import logger as main_logger
 from datetime import datetime
 from pathlib import Path
 
-# Log to: "./Logs/mitm_<timestamp>.log"
-log_path = Path().cwd() / "logs" / f"mitm_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
-logger: loguru.Logger = main_logger.bind(module="mitm")
-main_logger.add(log_path, level="DEBUG", filter=lambda record: record["extra"].get("module") == "mitm")
+# Log to: "./Logs/playwright_<timestamp>.log"
+log_path = Path().cwd() / "logs" / f"playwright_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+logger: loguru.Logger = main_logger.bind(module="playwright")
+main_logger.add(log_path, level="DEBUG", filter=lambda record: record["extra"].get("module") == "playwright")
